ae6b9e32e2c565c911e36c8b3a7264f7154e57ce
Van Hoang <21110828@student.hcmute.edu.vn>
2023-09-23 23:52:55 +0700

    initial project

---
+++ src/main/java/com/example/cinemachainmanagement/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.cinemachainmanagement;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CinemaChainManagementApplication.class);
+	}
+
+}
