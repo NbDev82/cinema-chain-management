c4eaaacb42579be918bf6442d708df95b602eccc
Tân <leetaan1902@gmail.com>
2023-11-26 22:38:16 +0700

    add movie

--- src/main/java/com/example/cinemachainmanagement/service/impl/MovieServiceImpl.java
+++ src/main/java/com/example/cinemachainmanagement/service/impl/MovieServiceImpl.java
@@ -1,11 +1,18 @@
 package com.example.cinemachainmanagement.service.impl;
 
+import com.example.cinemachainmanagement.DTO.MovieDTO;
+import com.example.cinemachainmanagement.DTO.ProductDTO;
+import com.example.cinemachainmanagement.Mapper.Mappers;
+import com.example.cinemachainmanagement.Mapper.ProductMapper;
 import com.example.cinemachainmanagement.entities.Movie;
+import com.example.cinemachainmanagement.entities.Product;
+import com.example.cinemachainmanagement.entities.Theater;
 import com.example.cinemachainmanagement.repositories.MovieRepository;
 import com.example.cinemachainmanagement.service.MovieService;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,5 +38,11 @@ public class MovieServiceImpl implements MovieService {
                 .filter(movie -> movie.getShowTimes().stream()
                         .anyMatch(showtime -> showtime.getDate().after(currentTime)))
                 .toList();
+    }
+
+    @Override
+    public List<MovieDTO> getListMovie() {
+        List<Movie> listmovie = movieRepository.findAll();
+        return Mappers.mapperEntityToDto(listmovie,MovieDTO.class);
     }
 }
