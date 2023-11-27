07239f92aef4039da1428195e9af8a514017f7c9
Tân <leetaan1902@gmail.com>
2023-11-27 19:46:14 +0700

    view home, folder image movie

--- src/main/java/com/example/cinemachainmanagement/controller/HomeController.java
+++ src/main/java/com/example/cinemachainmanagement/controller/HomeController.java
@@ -1,17 +1,29 @@
 package com.example.cinemachainmanagement.controller;
 
 
+import com.example.cinemachainmanagement.DTO.MovieDTO;
 import com.example.cinemachainmanagement.service.CustomerService;
+import com.example.cinemachainmanagement.service.MovieService;
+import com.example.cinemachainmanagement.service.TheaterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/home")
 public class HomeController {
     private final CustomerService customerService;
+
+    @Autowired
+    MovieService movieService;
+
+    @Autowired
+    TheaterService theaterService;
 
     public HomeController(CustomerService customerService) {
         this.customerService = customerService;
@@ -33,5 +45,18 @@ public class HomeController {
     @GetMapping("/signup")
     public String signup(){
         return "views/signup";
+    }
+
+
+    @GetMapping("")
+    public String getListMovie(Model model) {
+        try {
+            List<MovieDTO> movie_manager = movieService.getListMovie();
+            model.addAttribute("movie_manager", movie_manager);
+            return "home";
+        } catch (Exception e) {
+            model.addAttribute("error", "Lỗi khi load sản phẩm: " + e.getMessage());
+            return "error_view";
+        }
     }
 }
