945dadbf171cc434863be49183d88b6574a4a4b6
Van Hoang <21110828@student.hcmute.edu.vn>
2023-12-10 00:53:13 +0700

    Implement password hashing using SHA-256 algorithm

--- src/main/java/com/example/cinemachainmanagement/service/CustomerService.java
+++ src/main/java/com/example/cinemachainmanagement/service/CustomerService.java
@@ -2,9 +2,11 @@ package com.example.cinemachainmanagement.service;
 
 import com.example.cinemachainmanagement.DTO.CustomerDTO;
 import com.example.cinemachainmanagement.entities.Customer;
+import com.example.cinemachainmanagement.enums.EMessage;
 
 public interface CustomerService {
     Customer authenticateCustomer(String email, String password);
     boolean registerCustomer(CustomerDTO customerDTO);
     CustomerDTO getCustomerById(Long customerDTO_id);
+    EMessage changePassword(String email, String oldPassword, String newPassword);
 }
