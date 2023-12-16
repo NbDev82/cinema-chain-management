4d8c25ce41bdb07670d43761310a27d86fc08a56
Van Hoang <21110828@student.hcmute.edu.vn>
2023-12-17 00:59:34 +0700

    Implement: implement feature forgot password

--- src/main/java/com/example/cinemachainmanagement/controller/AuthenticationController.java
+++ src/main/java/com/example/cinemachainmanagement/controller/AuthenticationController.java
@@ -4,18 +4,24 @@ import com.example.cinemachainmanagement.DTO.CustomerDTO;
 import com.example.cinemachainmanagement.entities.Customer;
 import com.example.cinemachainmanagement.enums.EMessage;
 import com.example.cinemachainmanagement.enums.ERole;
+import com.example.cinemachainmanagement.generator.OTPGenerator;
+import com.example.cinemachainmanagement.service.AccountService;
 import com.example.cinemachainmanagement.service.CustomerService;
+import com.example.cinemachainmanagement.service.EmailService;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/customer_authentication")
 public class AuthenticationController {
-    @Autowired
-    CustomerService  customerService;
+    private final CustomerService  customerService;
+    private final AccountService accountService;
+    private final EmailService emailService;
     @GetMapping("/login")
     public String Login(Model model) {
         try {
@@ -91,5 +97,84 @@ public class AuthenticationController {
         EMessage result = customerService.changePassword(email, oldPassword, newPassword);
         model.addAttribute("result", result.getValue());
         return "passwordResult";
+    }
+
+    @GetMapping("/forgot-pass")
+    public String startForgotPass(){
+        try{
+            return "your-email";
+        }catch (Exception e){
+            return "404";
+        }
+    }
+
+    @PostMapping("/forgot-pass-result")
+    public String changePassword(@RequestParam String newPassword,
+                                 @RequestParam String confirmPassword,
+                                 HttpSession session,
+                                 Model model) {
+        String url = "login";
+        if(newPassword.equals(confirmPassword)){
+            String email = (String) session.getAttribute("email");
+            EMessage result = customerService.changePassword(email, newPassword);
+            model.addAttribute("result", result.getValue());
+        }else{
+            url = "forgot-pass";
+            model.addAttribute("result", EMessage.CONFIRM_PASSWORD_NOT_MATCH.getValue());
+        }
+        return url;
+    }
+
+    @PostMapping("/forgot-pass")
+    public String changePass(){
+        try{
+            return "forgot-pass";
+        }catch (Exception e){
+            return "404";
+        }
+    }
+
+    @GetMapping("/otp")
+    public String sendOTP(@RequestParam String email,
+                          HttpSession session,
+                          Model model){
+        try{
+            Customer customer = customerService.getCustomerByEmail(email);
+            if(customer == null){
+                model.addAttribute("result",EMessage.CUSTOMER_NOT_EXIST.getValue());
+                return "your-email";
+            }else{
+                String OTP = OTPGenerator.generateOTP();
+                String OTPHash = accountService.hashPassword(OTP);
+                emailService.sendEmail(email,EMessage.TITLE_OTP.getValue(),EMessage.TEXT_EMAIL_OTP.getValue()+OTP);
+                session.setAttribute("OTPHash",OTPHash);
+                session.setAttribute("email",email);
+                return "type-otp";
+            }
+        }catch (Exception e){
+            return "404";
+        }
+    }
+
+    @PostMapping("/otp")
+    public String checkOTP(@RequestParam String otp,
+                           HttpSession session,
+                           Model model){
+        try{
+            String url="forgot-pass";
+            String OTPHash = (String) session.getAttribute("OTPHash");
+            if(OTPHash == null)
+                OTPHash = url;
+            if(accountService.hashPassword(otp).equals(OTPHash)){
+                session.setAttribute("OTPHash",null);
+            }
+            else{
+                model.addAttribute("result",EMessage.OTP_NOT_MATCH.getValue());
+                url = "type-otp";
+            }
+            return url;
+        }catch (Exception e){
+            return "404";
+        }
     }
 }
